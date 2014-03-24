f94d7edafb1349ce238b2b7c22dcec28d16b535a
Scott O'Brien <scotto@uow.edu.au>
2014-03-24 13:47:53 +1100

    Added dashboard functionality.  Chart is now working

--- models/monthly_totals.js
+++ models/monthly_totals.js
@@ -1,6 +1,6 @@
 MonthlyTotals = new Meteor.Collection('monthly_totals')
 
-MonthlyTotals.yearlyTotals = function() {
+MonthlyTotals.yearlyTotals = function(year) {
 		//The year passed in is just a normal integer
 		if(typeof year === "undefined") {
 				year = moment().year()
@@ -8,9 +8,5 @@ MonthlyTotals.yearlyTotals = function() {
 		var start_date = new Date(year.toString())
 		var end_date = moment(start_date).add('year', 1).toDate()
 
-		console.log("Start Date: " + start_date);
-
 		return MonthlyTotals.find( {date: {$gte: start_date, $lt: end_date}} )
-
-		return MonthlyTotals;
 }
--- sections/dashboard.js
+++ sections/dashboard.js
@@ -8,30 +8,34 @@ Router.map(function() {
         before: function() {
             this.subscribe('monthly_totals_for_year').wait();
             this.subscribe('this_weeks_usage').wait();
+            this.subscribe('30day_usage').wait();
         },
 
         data: function() {
+
             var monthly_totals = MonthlyTotals.find();
             if (WeeklyTotals.find().count() > 0)
                 var weekly_usage = WeeklyTotals.findOne().communities;
             else
                 var weekly_usage = []
-            //debugger;
-            var to_return = {}
+
+
+            var monthly_comm_totals = {}
             monthly_totals.forEach(function(monthly_total) {
                 for(community in monthly_total.communities) {
-                    if (!(community in to_return)) {
-                        to_return[community] = 0;
+                    if (!(community in monthly_comm_totals)) {
+                        monthly_comm_totals[community] = 0;
                     }
-                    to_return[community] += monthly_total.communities[community]
+                    monthly_comm_totals[community] += monthly_total.communities[community]
                 }
             });
-            //for(monthly_total in monthly_totals) {
-            //}
+
+            var months_daily_totals = DailyTotals.find();
+
             var data ={
-                yearlyTotal: _.map(to_return, function(val,key){return {community: key, bytes: val}}),
+                yearlyTotal: _.map(monthly_comm_totals, function(val,key){return {community: key, bytes: val}}),
                 weeklyTotal: _.map(weekly_usage, function(val,key){return {community: key, bytes: val}}),
-                 
+                months_daily_totals: months_daily_totals,
             }
             return data;
         }	
@@ -39,8 +43,8 @@ Router.map(function() {
 })
 
 
-
 if (Meteor.isServer) {
+
     Meteor.publish("monthly_totals_for_year", function() {
         return MonthlyTotals.yearlyTotals(2014);
     });
@@ -48,10 +52,13 @@ if (Meteor.isServer) {
     Meteor.publish("this_weeks_usage", function() {
         return WeeklyTotals.find({}, { sort: {date: -1}, limit: 1 })
     });
+
+    Meteor.publish('30day_usage', function() {
+        return DailyTotals.last30days();
+    })
 }
 
 if (Meteor.isClient) {
-
     //Some helpers for formatting the values
     Template.totalValues.community = function() {
         //debugger;
@@ -66,35 +73,92 @@ if (Meteor.isClient) {
         return this.community
     }
     Template.totalValues.traffic = function() {
+//        return this.bytes;
         return readablizeBytes(this.bytes)
     }
 
-    Template.dashboard.rendered = function() {
 
-        $(function () { 
+    //Attach the rendering of the 30 day chart
+    Template.dashboard_30days.rendered = function() {
+        if(! $('#dailytraffic').highcharts() ) {
             $('#dailytraffic').highcharts({
                 chart: {
-                    type: 'bar'
+                    type: 'area'
                 },
                 title: {
-                    text: 'Fruit Consumption'
+                    text: '30 day traffic consumption'
                 },
                 xAxis: {
-                    categories: ['Apples', 'Bananas', 'Oranges']
+                    labels: {
+                        formatter: function() {
+                            return this.value; //Returns the data consumed for this day
+                        },
+                        rotation: -45,
+                        align: 'right',
+                        step: 2,
+                        style: {
+                            fontSize: '13px',
+                            fontFamily: 'Verdana, sans-serif'
+                        }
+                    }
                 },
                 yAxis: {
                     title: {
-                        text: 'Fruit eaten'
+                        text: 'Data Consumed'
+                    },
+               },
+                series: [{
+                    name: 'On-Net',
+                    data: []
+                }, {
+                    name: 'Off-Net',
+                    data: []
+                }],
+                tooltip: {
+                    formatter: function() {
+                        to_return = "<strong><h4>" + this.series.name + "</h4></strong><br />"
+                        to_return += this.point.day + "<br />"
+                        to_return += this.x + "<br />"
+                        to_return += readablizeBytes(this.y) + "<br />";
+                        return to_return;
                     }
                 },
-                series: [{
-                    name: 'Jane',
-                    data: [1, 0, 4]
-                }, {
-                    name: 'John',
-                    data: [5, 7, 3]
-                }]
+                credits: {
+                    enabled: false,
+                }
             });
-        });
+        }
+        else {
+            //The chart is already displayed. Time to update it with some data
+
+            on_net = [];
+            off_net = [];
+            categories = []
+
+            this.data.months_daily_totals.forEach(function(total) {
+                categories.push(moment(total.date).format("MMM Do YYYY"));
+                var day_of_week = moment(total.date).format('dddd');
+                on_net.push( {
+                    'y':  total.communities['58698:101'],
+                    day: day_of_week,
+                    
+                });
+                off_net.push( {
+                    'y':  total.communities['58698:102'],
+                    day: day_of_week,
+                })
+
+            })
+
+            var chart = $('#dailytraffic').highcharts();
+
+            chart.xAxis[0].setCategories(categories);
+            chart.series[0].update({
+                data: on_net,
+            });
+            chart.series[1].update({
+                data: off_net
+            });
+        }
     }
 }
